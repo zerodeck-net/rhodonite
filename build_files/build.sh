6572c30ae7151dca68e79dfbc23146b1dbeb69dd
LunaCOLON3 <lunacolon3@proton.me>
2025-09-06 20:17:52 -0700

    feat: Rhodonite

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+dnf5 remove plasma-desktop
+dnf5 install plasma-bigscreen
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
