b7d3f5c86cd4e8fd7f953f6d65b1285f5d263e7f
LunaCOLON3 <lunacolon3@proton.me>
2025-09-06 20:30:14 -0700

    fix: add -y flag to dnf5 commands

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
-dnf5 remove plasma-desktop
-dnf5 install plasma-bigscreen
+dnf5 remove -y plasma-desktop
+dnf5 install -y plasma-bigscreen
 
 # Use a COPR Example:
 #
